9a5739ea2b01c77ba69fb661b50d73a7c879ffb9
juancubides01 <60710834+juancubides01@users.noreply.github.com>
2020-02-21 14:31:07 -0500

    Add files via upload

---
+++ Sumatoria de numeros/src/sumatoria/de/numeros/SumatoriaDeNumeros.java
@@ -0,0 +1,20 @@
+
+package sumatoria.de.numeros;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+
+public class SumatoriaDeNumeros {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
