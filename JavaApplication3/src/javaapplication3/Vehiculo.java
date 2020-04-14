362afce186c35acf6486ab005924d089b9f2d9e7
juancubides01 <60710834+juancubides01@users.noreply.github.com>
2020-04-14 16:24:49 -0500

    Add files via upload

---
+++ JavaApplication3/src/javaapplication3/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+import java.util.Calendar;
+import java.util.Date;
+/**
+ *
+ * @author user
+ */
+public class Vehiculo {
+   Date tiempoingreso;
+   Date tiemposalida;
+   String tipodevehiculo;
+   Boolean vehiculoenparqueadero;
+}
---
+++ JavaApplication3/src/javaapplication3/objetoparqueadero.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author user
+ */
+public class objetoparqueadero {
+    float costoparqueaderocarros;
+    float costoparqueaderomotos;
+    float costoparqueaderobicicletas;
+    float maximacapacidacarros;
+    float maximacapacidamotos;
+    float maximacapacidabicicletas;
+    
+  objetoparqueadero(float p1,float p2,float p3,float p4,float p5,float p6){
+       this.costoparqueaderocarros=p1;
+       this.costoparqueaderomotos=p2;
+       this.costoparqueaderobicicletas=p3;
+       this.maximacapacidacarros=p4;
+       this.maximacapacidamotos=p5;
+       this.maximacapacidabicicletas=p6;
+   }
+}
